736c9e4be0a03396f4f637c28142fa1ac942fd6a
Eric Carlisle <eric@ericcarlisle.com>
2017-07-05 01:08:00 -0400

    Add image gallery check.

--- themes/podium/static/js/main.js
+++ themes/podium/static/js/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+if ($('.image-gallery').length) {
+
+	$('.image-gallery').magnificPopup({
+	delegate: 'a',
+	type: 'image',
+		gallery: {
+			enabled: true,
+			preload: [0, 1],
+			arrowMarkup: '<button title=\'%title%\' type=\'button\' class=\'mfp-arrow mfp-arrow-%dir%\'></button>',
+			navigateByImgClick: true,
+			tPrev: 'Previous (Left arrow key)',
+			tNext: 'Next (Right arrow key)',
+			tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter		},
+		},
+	    mainClass: 'mfp-img-mobile',
+	});
+}
